8a38b36e6eb1816864251e0da7e8c1948657f164
Kosuke Mizumura <mizumura.it@gmail.com>
2022-02-23 16:40:04 +0900

    initial

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
+  webpack: (conf) => {
+    conf.module.rules.push({
+      test: /\.(glsl|frag|vert)$/,
+      exclude: /node_modules/,
+      use: [
+        'glslify-import-loader',
+        'raw-loader',
+        'glslify-loader',
+      ],
+    })
+    return conf
+  },
 }
-
-module.exports = nextConfig
